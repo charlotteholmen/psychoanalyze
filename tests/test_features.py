71ee40487d47a9968f86f384b91485cf95f942be
schlich <ty.schlich@gmail.com>
2026-02-11 05:36:40 +0000

    refactor

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,6 +1,9 @@
+from psychoanalyze import psi
+
+
 class TestPsychometricFunction:
     def test_preparing_plot_of_psychometric_function(self) -> None:
-        from psychoanalyze import psi
+
 
         fig = psi.plot()
         assert fig.layout.yaxis.title.text == "percent_correct"
